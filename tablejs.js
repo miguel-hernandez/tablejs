46dbd03acb45572b5fefe51a2fb4f1d3a32fb6db
miguelhernandez <miguelhernandez@miguelhernandez>
2017-08-21 22:27:51 -0500

    indent code

--- tablejs.js
+++ tablejs.js
@@ -1,76 +1,76 @@
 function Grid(iddiv,columnas,arr_datos){
-      obj_grid = this;
+  obj_grid = this;
 
-      obj_grid.iddiv = iddiv;
-      obj_grid.columns = columnas;
-      obj_grid.arr_datos = arr_datos;
+  obj_grid.iddiv = iddiv;
+  obj_grid.columns = columnas;
+  obj_grid.arr_datos = arr_datos;
 
-      obj_grid.html  = "";
-      obj_grid.arr_row_selected = [];
+  obj_grid.html  = "";
+  obj_grid.arr_row_selected = [];
 
-      this.init  = function(){
-        $("#"+obj_grid.iddiv+ " tbody tr").each(function () {
-              $(this).css( {"background-color": "white", "font-size": "14px"} );
-        });
-      }// init()
+  this.init  = function(){
+    $("#"+obj_grid.iddiv+ " tbody tr").each(function () {
+      $(this).css( {"background-color": "white", "font-size": "14px"} );
+    });
+  }// init()
 
-      this.load = function(){
-        var html="";
-        obj_grid.init();
+  this.load = function(){
+    var html="";
+    obj_grid.init();
 
-            html += "<div class='table-responsive'>";
-            html += "<table id='idtable' class='table bordered condensed'>";
-            html += "<thead>";
-            html += "<tr class='info'>";
+    html += "<div class='table-responsive'>";
+    html += "<table id='idtable' class='table bordered condensed'>";
+    html += "<thead>";
+    html += "<tr class='info'>";
 
-            for (var i = 0; i<obj_grid.columns.length; i++) {
-                html += "<td id='"+obj_grid.columns[i]+"'>";
-                html += obj_grid.columns[i];
-                html += "</td>";
-            }
-            html += "</tr>";
-            html += "</thead>";
+    for (var i = 0; i<obj_grid.columns.length; i++) {
+      html += "<td id='"+obj_grid.columns[i]+"'>";
+      html += obj_grid.columns[i];
+      html += "</td>";
+    }
+    html += "</tr>";
+    html += "</thead>";
 
-            html += "<tbody>";
-            for (var i = 0; i<obj_grid.arr_datos.length; i++) {
-              html += "<tr>";
-                for (var j=0; j<obj_grid.columns.length; j++) {
-                  html += "<td id='"+obj_grid.columns[j]+"' data='"+obj_grid.arr_datos[i][obj_grid.columns[j]]+"'>";
-                  html += obj_grid.arr_datos[i][obj_grid.columns[j]];
-                  html += "</td>";
-                }
-              html += "</tr>";
-            }
+    html += "<tbody>";
+    for (var i = 0; i<obj_grid.arr_datos.length; i++) {
+      html += "<tr>";
+      for (var j=0; j<obj_grid.columns.length; j++) {
+        html += "<td id='"+obj_grid.columns[j]+"' data='"+obj_grid.arr_datos[i][obj_grid.columns[j]]+"'>";
+        html += obj_grid.arr_datos[i][obj_grid.columns[j]];
+        html += "</td>";
+      }
+      html += "</tr>";
+    }
 
-            html += "</tbody>";
-            html += "</table>";
-            html += "</div>";
+    html += "</tbody>";
+    html += "</table>";
+    html += "</div>";
 
-            obj_grid.html = html;
+    obj_grid.html = html;
 
-            obj_grid.finish();
+    obj_grid.finish();
 
-      }// load()
+  }// load()
 
-      this.finish = function(){
-        $("#"+obj_grid.iddiv).empty();
-        $("#"+obj_grid.iddiv).append(obj_grid.html);
-      }// load()
+  this.finish = function(){
+    $("#"+obj_grid.iddiv).empty();
+    $("#"+obj_grid.iddiv).append(obj_grid.html);
+  }// load()
 
-      this.get_row_selected = function(){
-        return obj_grid.arr_row_selected;
-      }// load()
+  this.get_row_selected = function(){
+    return obj_grid.arr_row_selected;
+  }// load()
 
-      $(document).on("click", "#"+obj_grid.iddiv+" tbody tr", function(e) {
-                obj_grid.init();
-                $(this).css( {"background-color": "#CEF6CE", "font-size": "16px"} );
-                var data = $(this).attr('id');
+  $(document).on("click", "#"+obj_grid.iddiv+" tbody tr", function(e) {
+    obj_grid.init();
+    $(this).css( {"background-color": "#CEF6CE", "font-size": "16px"} );
+    var data = $(this).attr('id');
 
-                $("#id_seleccionado").empty("");
-                $("#id_seleccionado").append(data);
+    $("#id_seleccionado").empty("");
+    $("#id_seleccionado").append(data);
 
-                 $(this).children("td").each(function (){
-                  obj_grid.arr_row_selected[this.id] = $(this).attr('data');
-                 });
-      });
+    $(this).children("td").each(function (){
+      obj_grid.arr_row_selected[this.id] = $(this).attr('data');
+    });
+  });
 }// Grid
--- test.js
+++ test.js
@@ -1,53 +1,53 @@
 $(document).ready(function () {
-        init();
+  init();
 
-         $("#btn_grid_edit").click(function(e){
-                e.preventDefault();
-                var arr_row = grid.get_row_selected();
-                show_selected(arr_row);
-         });
+  $("#btn_grid_edit").click(function(e){
+    e.preventDefault();
+    var arr_row = grid.get_row_selected();
+    show_selected(arr_row);
+  });
 
-         $("#btn_grid_delete").click(function(e){
-            e.preventDefault();
-            var arr_row = grid.get_row_selected();
-            show_selected(arr_row);
-         });
+  $("#btn_grid_delete").click(function(e){
+    e.preventDefault();
+    var arr_row = grid.get_row_selected();
+    show_selected(arr_row);
+  });
 });
 
 function init(){
-          $.ajax({
-              type: 'POST',
-              url: "Usuario.php",
-              data: {},
-              success: function (data) {
-                var arr_pgridok = JSON.parse(data);
-                columnas = ["id_usuario","nombre","paterno","materno","email"];
+  $.ajax({
+    type: 'POST',
+    url: "Usuario.php",
+    data: {},
+    success: function (data) {
+      var arr_pgridok = JSON.parse(data);
+      columnas = ["id_usuario","nombre","paterno","materno","email"];
 
-                grid = new Grid(
-                                                      "div_grid", // el id del div HTML
-                                                      columnas, // El array de columnas, serán los encabezados
-                                                      arr_pgridok // E array de los datos para llenar el grid, los índices deben corresponder a los nombres de las columnas
-                                                    );
-                grid.load();
+      grid = new Grid(
+        "div_grid", // el id del div HTML
+        columnas, // El array de columnas, serán los encabezados
+        arr_pgridok // E array de los datos para llenar el grid, los índices deben corresponder a los nombres de las columnas
+      );
+      grid.load();
 
-              },
-              error: function (data) {
-                  console.error("error...");
-              }
-          });
+    },
+    error: function (data) {
+      console.error("error...");
+    }
+  });
 }// init()
 
 function show_selected(row){
-            var arr_ = [];
-            var str = "";
-            arr_.push(row);
+  var arr_ = [];
+  var str = "";
+  arr_.push(row);
 
-            for(var i=0;i<arr_.length;i++){
-              for(var j=0;j<columnas.length;j++){
-                str = str+[columnas[j]]+": "+arr_[i][columnas[j]]+"<br>         ";
-              }
-            }
+  for(var i=0;i<arr_.length;i++){
+    for(var j=0;j<columnas.length;j++){
+      str = str+[columnas[j]]+": "+arr_[i][columnas[j]]+"<br>         ";
+    }
+  }
 
-            $("#fila_seleccionada").empty();
-            $("#fila_seleccionada").append(str);
+  $("#fila_seleccionada").empty();
+  $("#fila_seleccionada").append(str);
 }// show_selected()
