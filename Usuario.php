6eb19a0dd47a8643325dcaead82f19ce69003903
miguelhernandez <miguelhernandez@miguelhernandez>
2017-08-21 22:30:12 -0500

    PDO for usuario

--- Usuario.php
+++ Usuario.php
@@ -6,23 +6,15 @@ require_once("Connection.php");
 
  class Usuario {
 
-   private $login;
-   private $pass;
-   private $nombre;
-   private $paterno;
-   private $materno;
-
    const TABLA = 'usuario';
 
    public function __construct($login) {
-      $this->login = $login;
    }# __construct
 
     public static function  get_all(){
        try{
           $conexion = Conexion::getInstancia();
           $consulta = $conexion->prepare('SELECT * FROM ' . self::TABLA );
-           $consulta->bindParam(':login', $login);
            $consulta->execute();
            $usuarios = $consulta->fetchAll(PDO::FETCH_ASSOC);
            return $usuarios;
